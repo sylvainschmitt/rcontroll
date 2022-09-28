3040f153e18ad8135776b739a3935f6a3dd15cde
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2022-09-28 13:56:17 -0300

    add normalizePath for relative path

--- R/troll.R
+++ R/troll.R
@@ -112,6 +112,8 @@ troll <- function(name = NULL,
     path <- getOption("rcontroll.tmp")
     tmp <- TRUE
   }
+  if (!is.null(path))
+    path <- normalizePath(path)
   if (name %in% list.dirs(path, full.names = FALSE)[-1]) {
     if (!overwrite) {
       stop("Outputs already exist, use overwrite = TRUE.")
