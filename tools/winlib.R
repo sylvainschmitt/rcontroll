e223fc8a431a21f5da370c1ba8085f629ad6959c
gsalzet <55494561+gsalzet@users.noreply.github.com>
2022-12-18 18:46:54 +0100

    rcontroll 0.1.0.9056.8

--- tools/winlib.R
+++ tools/winlib.R
@@ -1,8 +1,8 @@
 version <- commandArgs(TRUE)
 
 # Download GSL Rtools build
-if (!file.exists(sprintf("../windows/gsl-%s/include/gsl/gsl_blas.h", version))){
-  download.file(sprintf("https://github.com/rwinlib/gsl/archive/v%s.zip", version), "lib.zip", quiet = TRUE)
+if (!file.exists(sprintf("../windows/gsl-%s/include/gsl/gsl_blas.h", as.character(version)))){
+  download.file(sprintf("https://github.com/rwinlib/gsl/archive/v%s.zip", as.character(version)), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
