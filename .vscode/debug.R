39880dbe7db05d3d8aa2370504389d71e1536d93
sylvainschmitt <sylvain.m.schmitt@gmail.com>
2023-08-08 09:06:30 +0200

    linted

--- .vscode/debug.R
+++ .vscode/debug.R
@@ -4,6 +4,6 @@ rnames <- envnames[startsWith(envnames, "R_")]
 cached_names <- rnames
 ld_lib_path <- Sys.getenv("LD_LIBRARY_PATH")
 if (ld_lib_path != "") {
-    cached_names <- c("LD_LIBRARY_PATH", rnames)
+  cached_names <- c("LD_LIBRARY_PATH", rnames)
 }
 writeLines(paste0(cached_names, "=", env[cached_names]), ".vscode/.env")
