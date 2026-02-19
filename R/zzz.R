f26815c6d7bacbf9fb5655d3b2787f01b63ef0fb
Théo Boiteux <theobtx987@gmail.com>
2026-02-19 16:38:04 +0100

    Troll version theo

--- R/zzz.R
+++ R/zzz.R
@@ -16,7 +16,7 @@ NULL
   dir.create(tmp_dir)
   options(list(
     rcontroll.tmp = tmp_dir,
-    rcontroll.troll = "TROLL version 3.1.8 Audrey"
+    rcontroll.troll = "TROLL version 3.1.8 Theo"
   ))
   invisible()
 }
